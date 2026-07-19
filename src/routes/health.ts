a0efd60006b339a20ae78579b6425d02cf517b30
Mohsin-Pix <mohsin.h@pixcodex.com>
2026-07-19 17:36:25 +0500

    learning docker

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -4,7 +4,7 @@ import type { Request, Response } from "express";
 const heathRouter = Router();
 
 heathRouter.get("/health", (_req: Request, res: Response) => {
-    res.status(200).json({success: true, message: "Server is healthy"});
+    res.status(200).json({success: true, message: "Server is healthy and running!"});
 });
 
 export default heathRouter;
