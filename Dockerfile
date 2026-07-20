61624590cc4e4954f75d7d80466dccee774e21f3
Mohsin-Pix <mohsin.h@pixcodex.com>
2026-07-20 10:07:11 +0500

    fix the issue realted to cicd to amke sure it works properlly

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM node:22-alpine
 WORKDIR /app    
 
 ENV PORT=8000
+ARG APP_VERSION=local
+ENV APP_VERSION=$APP_VERSION
 
 COPY package*.json ./
 
--- src/routes/health.ts
+++ src/routes/health.ts
@@ -4,7 +4,11 @@ import type { Request, Response } from "express";
 const heathRouter = Router();
 
 heathRouter.get("/health", (_req: Request, res: Response) => {
-    res.status(200).json({success: true, message: "Server is healthy, running and working!"});
+    res.status(200).json({
+        success: true,
+        message: "Server is healthy, running and working!",
+        version: process.env.APP_VERSION ?? "local",
+    });
 });
 
 export default heathRouter;
